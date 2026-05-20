ce2a90e0b81af51d4b7794e1ae0f42b31d314dc5
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-05-20 17:16:08 -0400

    Added devise for user auth disabled turbo so devise redirects to inertia pages

--- app/controllers/inertia_example_controller.rb
+++ app/controllers/inertia_example_controller.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class InertiaExampleController < InertiaController
+  before_action :authenticate_user!
+
   def index
     render inertia: {
       rails_version: Rails.version,
       ruby_version: RUBY_DESCRIPTION,
       rack_version: Rack.release,
-      inertia_rails_version: InertiaRails::VERSION,
+      inertia_rails_version: InertiaRails::VERSION
     }
+  end
+
+  def test
+    redirect_to inertia_example_url
   end
 end
--- app/javascript/pages/layouts/main_layout.jsx
+++ app/javascript/pages/layouts/main_layout.jsx
@@ -5,6 +5,7 @@ export default function MainLayout({children}) {
         <header style={{backgroundColor: "rebeccapurple"}}>
             <nav style={{display: "flex", alignItems: "center", justifyContent: "space-around", color: "white", fontSize: "1.5rem", fontWeight: "bolder"}}>
                 <Link href="/">Home</Link>
+                <Link href="/users/sign_out" method="delete">Logout</Link>
             </nav>
         </header>
         <main>

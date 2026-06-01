dd39d5b5b2e861168a6cce0d88b038516348e89d
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-06-01 14:47:33 -0400

    Added styling to users list page

--- app/javascript/pages/inertia_example/users.jsx
+++ app/javascript/pages/inertia_example/users.jsx
@@ -21,27 +21,30 @@ export default function Friends({auth, friends, incoming_friend_requests, outgoi
     }
 
     const accepted_friends = friends.map(friend => <div className="user friend" key={friend.id}>
-        <p>{friend.email}</p>
+        <h1>{friend.email}</h1>
         <button onClick={() => delete_friend(friend.id)}>Remove Friend</button>
     </div>)
     const incoming_friends = incoming_friend_requests.map(user => <div className="user pending" key={user.id}>
-        <p>{user.email}</p>
+        <h1>{user.email}</h1>
         <button onClick={() => add_friend(user.id)}>Accept Friend</button>
     </div>)
     const outgoing_friends = outgoing_friend_requests.map(user => <div className="user pending" key={user.id}>
-        <p>{user.email}</p>
+        <h1>{user.email}</h1>
         <button onClick={() => delete_friend(user.id)}>Cancel request</button>
     </div>)
     const other_users = users_no_requests.map(user => <div className="user no-request" key={user.id}>
-        <p>{user.email}</p>
+        <h1>{user.email}</h1>
         <button onClick={() => add_friend(user.id)}>Add Friend</button>
     </div>);
     return(<div id="test-content">
         <Head title="Users" />
-        {accepted_friends}
-        {incoming_friends}
-        {outgoing_friends}
-        {other_users}
+        <h1>All Users</h1>
+        <div className="user-list">
+            {accepted_friends}
+            {incoming_friends}
+            {outgoing_friends}
+            {other_users}
+        </div>
     </div>);
 }
 
